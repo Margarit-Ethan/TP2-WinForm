4e906d7b896eb643cff6f1c353968db4d9c5e504
Margarit-Ethan <153843557+Margarit-Ethan@users.noreply.github.com>
2024-03-19 10:30:23 +0100

    Add files via upload

---
+++ TP2WF/TP2WF/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TP2WF
+{
+    partial class Fm_Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lb_liste1 = new ListBox();
+            tb_saisie = new TextBox();
+            bt_ajout = new Button();
+            bt_suppr = new Button();
+            label1 = new Label();
+            lb_liste2 = new ListBox();
+            bt_suppr2 = new Button();
+            bt_ajout2 = new Button();
+            bt_switch1 = new Button();
+            bt_switch2 = new Button();
+            SuspendLayout();
+            // 
+            // lb_liste1
+            // 
+            lb_liste1.BackColor = SystemColors.ButtonFace;
+            lb_liste1.FormattingEnabled = true;
+            lb_liste1.ItemHeight = 20;
+            lb_liste1.Location = new Point(12, 72);
+            lb_liste1.Name = "lb_liste1";
+            lb_liste1.Size = new Size(546, 444);
+            lb_liste1.TabIndex = 0;
+            lb_liste1.SelectedIndexChanged += lb_liste1_SelectedIndexChanged;
+            lb_liste1.DoubleClick += lb_liste1_DoubleClick;
+            // 
+            // tb_saisie
+            // 
+            tb_saisie.BackColor = SystemColors.ButtonFace;
+            tb_saisie.Cursor = Cursors.Hand;
+            tb_saisie.Location = new Point(269, 532);
+            tb_saisie.Name = "tb_saisie";
+            tb_saisie.Size = new Size(592, 27);
+            tb_saisie.TabIndex = 1;
+            // 
+            // bt_ajout
+            // 
+            bt_ajout.BackColor = SystemColors.Control;
+            bt_ajout.Cursor = Cursors.Hand;
+            bt_ajout.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            bt_ajout.Location = new Point(12, 522);
+            bt_ajout.Name = "bt_ajout";
+            bt_ajout.Size = new Size(94, 37);
+            bt_ajout.TabIndex = 2;
+            bt_ajout.Text = "+";
+            bt_ajout.UseVisualStyleBackColor = false;
+            bt_ajout.Click += bt_ajout_Click;
+            // 
+            // bt_suppr
+            // 
+            bt_suppr.BackColor = SystemColors.Control;
+            bt_suppr.Cursor = Cursors.Hand;
+            bt_suppr.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            bt_suppr.Location = new Point(112, 522);
+            bt_suppr.Name = "bt_suppr";
+            bt_suppr.Size = new Size(94, 37);
+            bt_suppr.TabIndex = 3;
+            bt_suppr.Text = "-";
+            bt_suppr.UseVisualStyleBackColor = false;
+            bt_suppr.Click += bt_suppr_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("MS Reference Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(516, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 28);
+            label1.TabIndex = 5;
+            label1.Text = "LISTE";
+            // 
+            // lb_liste2
+            // 
+            lb_liste2.BackColor = SystemColors.ButtonFace;
+            lb_liste2.FormattingEnabled = true;
+            lb_liste2.ItemHeight = 20;
+            lb_liste2.Location = new Point(562, 72);
+            lb_liste2.Name = "lb_liste2";
+            lb_liste2.Size = new Size(546, 444);
+            lb_liste2.TabIndex = 6;
+            lb_liste2.SelectedIndexChanged += lb_liste2_SelectedIndexChanged;
+            lb_liste2.DoubleClick += lb_liste2_DoubleClick;
+            // 
+            // bt_suppr2
+            // 
+            bt_suppr2.BackColor = SystemColors.Control;
+            bt_suppr2.Cursor = Cursors.Hand;
+            bt_suppr2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            bt_suppr2.ImageAlign = ContentAlignment.TopLeft;
+            bt_suppr2.Location = new Point(914, 522);
+            bt_suppr2.Name = "bt_suppr2";
+            bt_suppr2.Size = new Size(94, 37);
+            bt_suppr2.TabIndex = 7;
+            bt_suppr2.Text = "-";
+            bt_suppr2.UseVisualStyleBackColor = false;
+            bt_suppr2.Click += bt_suppr2_Click;
+            // 
+            // bt_ajout2
+            // 
+            bt_ajout2.BackColor = SystemColors.Control;
+            bt_ajout2.Cursor = Cursors.Hand;
+            bt_ajout2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            bt_ajout2.ImageAlign = ContentAlignment.TopCenter;
+            bt_ajout2.Location = new Point(1014, 522);
+            bt_ajout2.Name = "bt_ajout2";
+            bt_ajout2.Size = new Size(94, 37);
+            bt_ajout2.TabIndex = 8;
+            bt_ajout2.Text = "+";
+            bt_ajout2.UseVisualStyleBackColor = false;
+            bt_ajout2.Click += bt_ajout2_Click;
+            // 
+            // bt_switch1
+            // 
+            bt_switch1.Cursor = Cursors.Hand;
+            bt_switch1.Location = new Point(63, 565);
+            bt_switch1.Name = "bt_switch1";
+            bt_switch1.Size = new Size(94, 29);
+            bt_switch1.TabIndex = 9;
+            bt_switch1.Text = ">>";
+            bt_switch1.UseVisualStyleBackColor = true;
+            bt_switch1.Click += bt_switch1_Click;
+            // 
+            // bt_switch2
+            // 
+            bt_switch2.Cursor = Cursors.Hand;
+            bt_switch2.Location = new Point(965, 565);
+            bt_switch2.Name = "bt_switch2";
+            bt_switch2.Size = new Size(94, 29);
+            bt_switch2.TabIndex = 10;
+            bt_switch2.Text = "<<";
+            bt_switch2.UseVisualStyleBackColor = true;
+            bt_switch2.Click += bt_switch2_Click;
+            // 
+            // Fm_Principal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Tan;
+            ClientSize = new Size(1120, 632);
+            Controls.Add(bt_switch2);
+            Controls.Add(bt_switch1);
+            Controls.Add(bt_ajout2);
+            Controls.Add(bt_suppr2);
+            Controls.Add(lb_liste2);
+            Controls.Add(label1);
+            Controls.Add(bt_suppr);
+            Controls.Add(bt_ajout);
+            Controls.Add(tb_saisie);
+            Controls.Add(lb_liste1);
+            ForeColor = Color.Black;
+            Name = "Fm_Principal";
+            Text = "Premier programme";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lb_liste1;
+        private TextBox tb_saisie;
+        private Button bt_ajout;
+        private Button bt_suppr;
+        private Label label1;
+        private ListBox lb_liste2;
+        private Button bt_suppr2;
+        private Button bt_ajout2;
+        private Button bt_switch1;
+        private Button bt_switch2;
+    }
+}
---
+++ TP2WF/TP2WF/Form1.cs
@@ -0,0 +1,113 @@
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TP2WF
+{
+    public partial class Fm_Principal : Form
+    {
+        public Fm_Principal()
+        {
+            InitializeComponent();
+            bt_switch1.Visible = false;
+            bt_switch2.Visible = false;
+            bt_switch1.Enabled = false;
+            bt_switch2.Enabled = false;
+        }
+
+        private void bt_ajout_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_saisie.Text))
+            {
+                MessageBox.Show("Veuillez saisir un élément.");
+            }
+            else
+            {
+                lb_liste1.Items.Add(tb_saisie.Text);
+                tb_saisie.Clear();
+                tb_saisie.Focus();
+            }
+        }
+        private void bt_suppr_Click(object sender, EventArgs e)
+        {
+            lb_liste1.Items.RemoveAt(lb_liste1.SelectedIndex);
+        }
+        private void bt_ajout2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_saisie.Text))
+            {
+                MessageBox.Show("Veuillez saisir un élément.");
+            }
+            else
+            {
+                lb_liste2.Items.Add(tb_saisie.Text);
+                tb_saisie.Clear();
+                tb_saisie.Focus();
+            }
+        }
+
+        private void bt_suppr2_Click(object sender, EventArgs e)
+        {
+            lb_liste2.Items.RemoveAt(lb_liste2.SelectedIndex);
+        }
+
+        private void bt_switch1_Click(object sender, EventArgs e)
+        {
+            if (lb_liste1.SelectedIndex != -1)
+            {
+                foreach (var selectedItem in lb_liste1.SelectedItems.Cast<string>().ToList())
+                {
+                    lb_liste2.Items.Add(selectedItem);
+                    lb_liste1.Items.Remove(selectedItem);
+                }
+            }
+        }
+
+        private void bt_switch2_Click(object sender, EventArgs e)
+        {
+            if (lb_liste2.SelectedIndex != -1)
+            { //switch de l'autre coté 
+                foreach (var selectedItem in lb_liste2.SelectedItems.Cast<string>().ToList())
+                {
+                    lb_liste1.Items.Add(selectedItem);
+                    lb_liste2.Items.Remove(selectedItem);
+                }
+            }
+        }
+        private void lb_liste1_DoubleClick(object sender, EventArgs e)
+        {
+            // Doubleclic pour tranfere
+
+            bt_switch1.PerformClick();
+        }
+        private void lb_liste2_DoubleClick(object sender, EventArgs e)
+        {
+            bt_switch2.PerformClick();
+        }
+
+        private void lb_liste1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bt_switch1.Enabled = (lb_liste1.SelectedIndex != -1);
+            if (lb_liste1.SelectedIndex != -1)
+            {
+                bt_switch1.Visible = true;
+            }
+            else
+            {
+                bt_switch1.Visible = false;
+            }
+        }
+
+        private void lb_liste2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bt_switch2.Enabled = (lb_liste2.SelectedIndex != -1);
+            if (lb_liste2.SelectedIndex != -1)
+            {
+                bt_switch2.Visible = true;
+            }
+            else
+            {
+                bt_switch2.Visible = false;
+            }
+        }
+    }
+}
